89bcb17c0cd0a763fa451e5c3d191df4729ebe79
hahihiha <bagusfajar891@gmail.com>
2024-01-31 05:02:32 +0700

    Merubah UI pada halaman

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../../../routes/app_pages.dart';
@@ -7,20 +6,66 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    double buttonWidth = MediaQuery.of(context).size.width * 0.7;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
+        backgroundColor: Color(0xFF0CBD63),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: ()=>Get.toNamed(Routes.BOOK), child: Text("Buku")),
-            ElevatedButton(onPressed: ()=>Get.toNamed(Routes.PEMINJAMAN), child: Text("Peminjaman")),
+            Container(
+              width: buttonWidth,
+              child: ElevatedButton(
+                onPressed: () => Get.toNamed(Routes.BOOK),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF0CBD63),
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+                child: Text(
+                  "Buku",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: buttonWidth,
+              child: ElevatedButton(
+                onPressed: () => Get.toNamed(Routes.PEMINJAMAN),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF0CBD63),
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+                child: Text(
+                  "Peminjaman",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ],
-        )
+        ),
       ),
     );
   }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -11,41 +11,94 @@ class LoginView extends GetView<LoginController> {
       appBar: AppBar(
         title: const Text('LoginView'),
         centerTitle: true,
+        backgroundColor: Color(0xFF0CBD63),
       ),
-      body: Center(
-        child: Form(
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        color: Colors.white, // Set background color to white
+        child: Center(
+          child: Form(
             key: controller.formKey,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              TextFormField(
-              controller: controller.usernameController,
-              decoration: InputDecoration(hintText: "Masukkan Username"),
-              validator: (value) {
-                if (value!.length < 2) {
-                  return "Username tidak boleh kosong";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: controller.passwordController,
-              decoration: InputDecoration(hintText: "Masukkan Username"),
-              validator: (value) {
-                if (value!.length < 2) {
-                  return "Password tidak boleh kosong";
-                }
-                return null;
-              },
-            ),
-            Obx(() =>
-            controller.loading.value ? CircularProgressIndicator()
-                : ElevatedButton(
-                onPressed: () {
-                  controller.login();
-                },
-                child: Text("Login")))
+                TextFormField(
+                  controller: controller.usernameController,
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    hintText: "Masukkan Username",
+                    prefixIcon: Icon(Icons.person),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF0CBD63)),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF0CBD63)),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Username tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: controller.passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Masukkan Password",
+                    prefixIcon: Icon(Icons.lock),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF0CBD63)),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF0CBD63)),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.length < 2) {
+                      return "Password tidak boleh kosong";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 32),
+                Obx(
+                  () => controller.loading.value
+                      ? CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Color(0xFF0CBD63)),
+                        )
+                      : ElevatedButton(
+                          onPressed: () {
+                            controller.login();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF0CBD63),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 40.0, // Increase horizontal padding
+                              vertical: 12.0,
+                            ),
+                          ),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white
+                            ),
+                          ),
+                        ),
+                ),
               ],
-            )),
+            ),
+          ),
+        ),
       ),
     );
   }
