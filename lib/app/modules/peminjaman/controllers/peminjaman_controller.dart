9e7300188c4fe860745c7858cf22f74cd36918bc
hahihiha <bagusfajar891@gmail.com>
2024-01-31 15:01:36 +0700

    menampilkan data peminjaman

--- lib/app/modules/peminjaman/controllers/peminjaman_controller.dart
+++ lib/app/modules/peminjaman/controllers/peminjaman_controller.dart
@@ -1,12 +1,17 @@
+import 'package:dio/dio.dart%20';
 import 'package:get/get.dart';
+import '../../../data/constant/endpoint.dart';
+import '../../../data/model/response_pinjam.dart';
+import '../../../data/provider/api_provider.dart';
 
-class PeminjamanController extends GetxController {
+class PeminjamanController extends GetxController with StateMixin<List<DataPinjam>> {
   //TODO: Implement PeminjamanController
 
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    Pinjam();
   }
 
   @override
@@ -18,6 +23,31 @@ class PeminjamanController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
+  Pinjam() async {
+    change(null, status: RxStatus.loading());
+    try {
+      final response = await ApiProvider.instance().get(Endpoint.pinjam);
+      if (response.statusCode == 200) {
+        final ResponsePinjam responePinjam = ResponsePinjam.fromJson(response.data);
+        if(responePinjam.data!.isEmpty){
+          change(null, status: RxStatus.empty());
+        } else {
+          change(responePinjam.data, status: RxStatus.success());
+        }
+      } else {
+        change(null, status: RxStatus.error("Gagal mengambil data"));
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          change(null, status: RxStatus.error("${e.response?.data['message']}"));
+        }
+      } else {
+        change(null, status: RxStatus.error(e.message ?? ""));
+      }
+    } catch (e) {
+      change(null, status: RxStatus.error(e.toString()));
+    }
+  }
   void increment() => count.value++;
 }
--- lib/app/modules/peminjaman/views/peminjaman_view.dart
+++ lib/app/modules/peminjaman/views/peminjaman_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../data/model/response_pinjam.dart';
 import '../controllers/peminjaman_controller.dart';
 
 class PeminjamanView extends GetView<PeminjamanController> {
@@ -13,12 +14,21 @@ class PeminjamanView extends GetView<PeminjamanController> {
         title: const Text('PeminjamanView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'PeminjamanView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: controller.obx((state) => ListView.separated(
+        itemCount: state!.length,
+        itemBuilder: (context, index) {
+          DataPinjam dataPinjam = state[index];
+          return ListTile(
+            title: Text("${state[index].book?.judul}"),
+            subtitle: Text(
+                "Peminjam : ${state[index].user?.nama}"
+                    "\nTanggal Pinjam : ${dataPinjam.tanggalPinjam}"
+                    "\nTanggal Kembali : ${dataPinjam.tanggalKembali}"
+                    "\nStatus : ${dataPinjam.status}"),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(),
+      )),
     );
   }
 }
